4caaba3accfe6e1407f0b0f8f09f99e2d3e62a2b
Himanshu Kumar <halp.raj2572000@gmail.com>
2023-12-26 17:36:17 +0530

    remove console

--- src/API.js
+++ src/API.js
@@ -2,7 +2,6 @@ import axios from "axios"
 export const getCapsule = () => {
     return axios.get('https://api.spacexdata.com/v3/capsules').then((res) =>{
         if(res?.data?.length > 0){
-            console.log("reeees:", res);
             return res.data
         } else {
             return []
